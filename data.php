2bc7f0f119d5206ddba34563e3aa6ed07704f757
Marcio de Carvalho <marciodecarvalho@ig.com.br>
2013-02-14 10:04:20 -0200

    Alterei cabeçalho com utf-8

--- data.php
+++ data.php
@@ -1,6 +1,10 @@
-<?php
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <?php
 
 echo date('d') . '/' . date('m') . '/' . date('y');
 echo '<br><hr><br>';
 
+ echo 'Hoje é ';
 echo date('d/m/y');
+echo '<br><hr><br>';
+
